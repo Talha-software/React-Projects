a0c4852509a7533f71f65cb0dd4b66fdf1a2a20e
Talha Sajjad <talha.sajjad.software@gmail.com>
2024-06-03 16:09:38 +0500

    muliti selection acoordian

--- react-projects/src/components/accordian/index.jsx
+++ react-projects/src/components/accordian/index.jsx
@@ -5,28 +5,43 @@ import "./style.css";
 
 export default function Accordian() {
     const [selected, setSelected] = useState(null);
-    function handleSinleSelection(getCurrentId){
-        setSelected(getCurrentId == selected ? null: getCurrentId)
+    const [enableMulitiSelection, setEnableMultiSelection] = useState(false)
+    const [multiple, setMultiple] = useState([])
+    function handleSinleSelection(getCurrentId) {
+        setSelected(getCurrentId == selected ? null : getCurrentId)
     }
-    console.log(selected)
+    function handleMultiSelection(getCurrentId) {
+        let cpyMutiple = [...multiple];
+        const findIndexOfCurrentId = cpyMutiple.indexOf(getCurrentId);
+
+        console.log(findIndexOfCurrentId);
+        if (findIndexOfCurrentId === -1) cpyMutiple.push(getCurrentId);
+        else cpyMutiple.splice(findIndexOfCurrentId, 1);
+
+        setMultiple(cpyMutiple);
+    }
+    console.log(multiple)
     return (
         <div className="acc-wrapper">
+            <button onClick={() => setEnableMultiSelection(!enableMulitiSelection)}>Enable multi selection</button>
             <div className="accordian">
                 {
                     data && data.length > 0 ? (
                         data.map((dataItem) => (
                             <div className="item">
-                                <div className="title" onClick={()=>handleSinleSelection(dataItem.id)}>
+                                <div className="title" onClick={enableMulitiSelection ? () => handleMultiSelection(dataItem.id) : () => handleSinleSelection(dataItem.id)}>
                                     <div className="title-1">
-                                    <h3>{dataItem.question}</h3>
-                                    <span>+</span>
+                                        <h3>{dataItem.question}</h3>
+                                        <span>+</span>
                                     </div>
                                     <div>
-                                        {
-                                            selected == dataItem.id ? <div className="acc-content">{dataItem.answer}</div>:null
-                                        }
-                                        </div>
+                                        {selected === dataItem.id ||
+                                            multiple.indexOf(dataItem.id) !== -1 ? (
+                                            <div className="acc-content">{dataItem.answer}</div>
+                                        ) : null}
+                                    </div>
                                 </div>
+
                             </div>
                         ))
                     )
